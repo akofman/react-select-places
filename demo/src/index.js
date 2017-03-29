7b38faa3d183379f87b8bf489e6c2e573e0e9050
Alexis Kofman <alexis.kofman@gmail.com>
2017-03-29 23:03:02 +0200

    Add Places info on change

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src'
+import SelectPlaces from '../../src'
 import 'react-select/dist/react-select.css'
 
-let Demo = React.createClass({
+class Demo extends Component {
+  onChange = (value) => {
+    console.log(value);
+  }
+
   render() {
     return <div>
       <h1>react-select-places Demo</h1>
-      <Component/>
+      <SelectPlaces onChange={this.onChange}/>
     </div>
   }
-})
+}
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -3,40 +3,40 @@ import Select from 'react-select';
 import scriptjs from 'scriptjs';
 
 class SelectPlaces extends Component {
-  static defaultProps = {
-    value: null
-  }
-
   static propTypes = {
     value: React.PropTypes.string
   }
 
-  state = {
-    value: this.props.value,
-  }
-
-  getPlacePredictions = (input, callback) => {
-    this.autocompleteService.getPlacePredictions({...this.props.autocompletionRequest, input}, (predictions) => {
-      let options = [];
-      if(predictions) {
-        options = predictions.map(prediction => ({
-          label: prediction.description
-        }));
-      }
-      callback(null, {
-        options,
-        complete: false
-      });
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    }
   }
 
   loadOptions = (input, callback) => {
+    const getPlacePredictions = (input, callback) => {
+      this.autocompleteService.getPlacePredictions({...this.props.autocompletionRequest, input}, (predictions) => {
+        let options = [];
+        if(predictions) {
+          options = predictions.map(prediction => ({
+            label: prediction.description,
+            placeId: prediction.place_id
+          }));
+        }
+        callback(null, {
+          options,
+          complete: false
+        });
+      });
+    };
+
     if (input) {
       if (!this.autocompleteService) {
         scriptjs(`https://maps.googleapis.com/maps/api/js?libraries=places&language=${this.props.language}&no-cache=${Math.random()}`, () => {
           if (window.google) {
             this.autocompleteService = new window.google.maps.places.AutocompleteService();
-            this.getPlacePredictions(input, callback);
+            getPlacePredictions(input, callback);
           }
           else {
             callback(null, {options: [], complete: false});
@@ -44,7 +44,7 @@ class SelectPlaces extends Component {
         });
       }
       else {
-        this.getPlacePredictions(input, callback);
+        getPlacePredictions(input, callback);
       }
     } else {
       callback(null, {options: [], complete: false});
@@ -52,15 +52,30 @@ class SelectPlaces extends Component {
   }
 
   onChange = value => {
-    this.setState({
-      value
-    });
+    const getDetails = (value) => {
+      this.placesService.getDetails({placeId: value.placeId}, (placeResult) => {
+        this.setState({
+          value
+        }, () => {
+          this.props.onChange && this.props.onChange({ ...value, ...placeResult});
+        });
+      });
+    }
+
+    if(window.google && !this.placesService){
+      this.placesService = new google.maps.places.PlacesService(this.refs.selectPlaces);
+      getDetails(value);
+    }
+    else {
+      getDetails(value);
+    }
   }
 
   render() {
     return (
       <div>
-        <Select.Async value={this.state.value} loadOptions={this.loadOptions} onChange={this.onChange} {...this.props.optionsForSelect} />
+        <Select.Async {...this.props} value={this.state.value} loadOptions={this.loadOptions} onChange={this.onChange} />
+        <div ref='selectPlaces'></div>
       </div>
     )
   }
